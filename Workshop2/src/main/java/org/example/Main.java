62eb23c82ebc334e444b058a2d85a17e7fd5e782
dominik <dominik.bin12@wp.pl>
2023-05-20 14:53:57 +0200

    konsola do testów

--- Workshop2/src/main/java/org/example/Main.java
+++ Workshop2/src/main/java/org/example/Main.java
@@ -1,18 +1,61 @@
 package org.example;
 
+import java.net.PasswordAuthentication;
 import java.sql.SQLException;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        printtasks(tasks);
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            switch (input) {
+                case "exit":
+                    System.exit(0);
+                    break;
+                case "create":
+                    System.out.println("Podaj Nazwę, email, hasło");
+                    createuser(scanner.nextLine(),scanner.nextLine(),scanner.nextLine());
+                    break;
+                case "read":
+                    System.out.println("Podaj Id");
+                    readuser(scanner.nextInt());
+                    break;
+                default:
+                    System.out.println("Wybierz poprawne zadanie");
+            }
+            printtasks(tasks);
+        }
+
+
+
+    }
+    static final String[] tasks = {"create","read", "exit"};
+    public static void printtasks(String[] tab) {
+        System.out.println("wybierz zadanie:");
+        for (String task : tab) {
+            System.out.println(task);
+        }
+    }
+
+    public static void readuser(int userid) {
         UserDao userDao = new UserDao();
-        User user = UserDao.read(2);
+        User user = UserDao.read(userid);
         System.out.println(user.getId());
         System.out.println(user.getUserName());
         System.out.println(user.getEmail());
         System.out.println(user.getPassword());
+    }
 
-
-
+    public static void createuser(String username, String emai, String password) {
+        UserDao userDao = new UserDao();
+        User user = new User();
+        user.setUserName(username);
+        user.setEmail(emai);
+        user.setPassword(password);
+        userDao.create(user);
     }
 }
--- Workshop2/src/main/java/org/example/UserDao.java
+++ Workshop2/src/main/java/org/example/UserDao.java
@@ -8,18 +8,22 @@ import java.util.Arrays;
 public class UserDao {
     private static final String CREATE_USER_QERY = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
 
-    public User create(User user) throws SQLException {
-        Connection conn = DbUtil.getConnection();
-        PreparedStatement statement = conn.prepareStatement(CREATE_USER_QERY,PreparedStatement.RETURN_GENERATED_KEYS);
-        statement.setString(1, user.getUserName());
-        statement.setString(2,user.getEmail());
-        statement.setString(3,hashPassword(user.getPassword()));
-        statement.executeUpdate();
-        ResultSet resultSet = statement.getGeneratedKeys();
-        if (resultSet.next()) {
-            user.setId(resultSet.getInt(1));
+    public User create(User user)  {
+        try (Connection conn = DbUtil.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(CREATE_USER_QERY, PreparedStatement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getUserName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, hashPassword(user.getPassword()));
+            statement.executeUpdate();
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if (resultSet.next()) {
+                user.setId(resultSet.getInt(1));
+            }
+            return user;
+        }catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
         }
-        return user;
     }
 
     public String hashPassword(String passwor) {
