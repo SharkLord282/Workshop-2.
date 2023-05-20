b4c166df80b592ac66dcd75f36001fe0f01b0833
dominik <dominik.bin12@wp.pl>
2023-05-20 12:18:03 +0200

    klasa user

--- Workshop2/src/main/java/org/example/User.java
+++ Workshop2/src/main/java/org/example/User.java
@@ -1,5 +1,40 @@
 package org.example;
 
 public class User {
+private static int id;
+private static String userName;
+private static String email;
+private static String password;
 
+    public static void setId(int id) {
+        User.id = id;
+    }
+
+    public static void setUserName(String userName) {
+        User.userName = userName;
+    }
+
+    public static void setEmail(String email) {
+        User.email = email;
+    }
+
+    public static void setPassword(String password) {
+        User.password = password;
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public static String getUserName() {
+        return userName;
+    }
+
+    public static String getEmail() {
+        return email;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
 }
